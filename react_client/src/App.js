7e0e8fa45a18b2c161f558f68d9ac3e3b2e2400f
Andrew Alten <altenfreelance@gmail.com>
2021-08-31 22:37:31 -0400

    Extracted auth hook and added auto refreshing

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -2,10 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container, Row, Button } from 'react-bootstrap'
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import jwt_decode from "jwt-decode";
 import { CopyBlock, dracula } from "react-code-blocks";
-var axios = require("axios").default;
+import useAuthCodeFlowPKCE from './hooks/useAuthCodeFlowPKCE';
 
 
 const authorizeEndpoint = process.env.REACT_APP_AUTH_ENDPOINT
@@ -18,131 +18,40 @@ const apiProtectedEndpoint = process.env.REACT_APP_PROTECTED_ROUTE
 function App() {
 
   const [jwtMsg, setJwtMsg] = useState(null)
-  const [refreshMsg, setRefreshMsg] = useState(null)
-  const [jwt, setJwt] = useState(null)
-  const [refreshToken, setRefreshToken] = useState(null)
-  const [jwtExpirationDate, setJwtExpirationDate] = useState(null)
   const [pocJson, setPocJson] = useState(null)
-
   const [tokenIsExpired, setTokenIsExpired] = useState(null)
 
-  useEffect(()=>{
-    var args = new URLSearchParams(window.location.search);
-      var code = args.get("code");
-
-    if (!code){
-
-      startOauthFlow()
-    }
-  
-  },[])
+  const [jwt, jwtExpiration, refreshJwtAndDoCallback] = useAuthCodeFlowPKCE(clientId, audience, authorizeEndpoint, tokenEndpoint)
+  const jwtRef = useRef(null)
 
   useEffect(() => {
-    if (window.location.search) {
-      var args = new URLSearchParams(window.location.search);
-      var code = args.get("code");
+    if (jwt) {
+      jwtRef.current = jwt
+      const decodedJwt = jwt_decode(jwt)
 
-      if (code) {
-       
-        var accessTokenOptions = {
-          method: 'POST',
-          url: tokenEndpoint,
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded', Accept: '*/*' },
-          data: `client_id=${clientId}&code_verifier=${window.sessionStorage.getItem("code_verifier")}&grant_type=authorization_code&redirect_uri=${window.location.href.replace(window.location.search, '')}&code=${code}`
-        };
-    
-        axios.request(accessTokenOptions)
-          .then(function (response) {
-    
-            const { access_token, refresh_token } = response.data
-
-            setTheTokens(access_token, refresh_token)
-          
-          }).catch(function (error) {
-            console.log(error)
-
-            window.location.href = window.location.href.replace(window.location.search, '')
-          });
-      }
+      setJwtMsg(JSON.stringify(decodedJwt).replace(/,/g, `,\n    `).replace(/{/g, `{\n    `).replace(/}/g, `\n}`))
     }
-  }, [])
+  }, [jwt])
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const now = Date.now()
-      setTokenIsExpired(jwtExpirationDate < now)
+      setTokenIsExpired(isTokenExpired())
     }, 1000);
     return () => clearInterval(interval);
-  }, [jwtExpirationDate]);
+  }, [jwtExpiration]);
 
-  function setTheTokens(access_token, refresh_token){
-    setJwt(access_token)
-    setRefreshToken(refresh_token)
-
-    const decodedJwt = jwt_decode(access_token)
-
-    const expDate = new Date(0);
-    expDate.setUTCSeconds(decodedJwt.exp);
-    setJwtExpirationDate(expDate)
-
-    // put attrs on new lines
-    setJwtMsg(JSON.stringify(decodedJwt).replace(/,/g, `,\n    `).replace(/{/g, `{\n    `).replace(/}/g, `\n}`))
-    setRefreshMsg(refresh_token)
+  function isTokenExpired() {
+    return jwtExpiration < Date.now()
   }
 
-  function refreshJwtAxios() {
-
-    var refreshOptions = {
-      method: 'POST',
-      url: tokenEndpoint,
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded', Accept: '*/*' },
-      data: `grant_type=refresh_token&client_id=${clientId}&refresh_token=${refreshToken}`
-    };
-
-    axios.request(refreshOptions)
-      .then(function (response) {
-        console.log(response, "response")
-
-        const { access_token, refresh_token } = response.data
-
-        setTheTokens(access_token, refresh_token)
-
-      }).catch(function (error) {
-        console.error(error);
-        alert(error)
-      });
-  }
-
-
-  function generateRandomString(length) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    return text;
-  }
-
-  async function generateCodeChallenge(codeVerifier) {
-    var digest = await crypto.subtle.digest("SHA-256",
-      new TextEncoder().encode(codeVerifier));
-
-    return btoa(String.fromCharCode(...new Uint8Array(digest)))
-      .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
-  }
-
-  function hitProtectedEndpoint() {
-    setPocJson(null)
-
+  function _doFetchProtectedData(token) {
     fetch(apiProtectedEndpoint, {
       method: "GET",
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
         Accept: "applicaiton/json",
-        Authorization: `Bearer ${jwt}`
+        Authorization: `Bearer ${token}`
       }
     })
       .then(res => res.json())
@@ -153,34 +62,27 @@ function App() {
       })
   }
 
+  function fetchProtectedData(token) {
+    setPocJson(null)
+
+    if (isTokenExpired()) {
+      refreshJwtAndDoCallback(_doFetchProtectedData)
+    }
+    else {
+      _doFetchProtectedData(token)
+    }
+
+
+  }
+
   function getPrettyDate(date) {
     return date.getMonth() + 1 + '/' + date.getDate() + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes()
   }
 
-  function startOauthFlow() {
-    var codeVerifier = generateRandomString(64);
-
-    generateCodeChallenge(codeVerifier).then(function (codeChallenge) {
-      // setCode(codeChallenge)
-      window.sessionStorage.setItem("code_verifier", codeVerifier);
-
-      var redirectUri = window.location.href.split('?')[0];
-      var args = new URLSearchParams({
-        response_type: "code",
-        client_id: clientId,
-        code_challenge_method: "S256",
-        code_challenge: codeChallenge,
-        redirect_uri: redirectUri,
-        audience: audience,
-        scope: "offline_access" //To enable refresh token
-      });
-      window.location = authorizeEndpoint + "/?" + args;
-    });
-  }
 
   return (
     <Container>
-      {jwtMsg && jwtExpirationDate &&
+      {jwtMsg && jwtExpiration &&
         <Row>
           <h1>JWT</h1>
           <CopyBlock
@@ -190,31 +92,30 @@ function App() {
             theme={dracula}
             codeBlock
           />
-          <p>Expiration Date: <b>{getPrettyDate(jwtExpirationDate)}</b></p>
+          <p>Expiration Date: <b>{getPrettyDate(jwtExpiration)}</b></p>
           {tokenIsExpired !== null && <p>Token is Expired: <b>{tokenIsExpired.toString()}</b></p>}
-          {tokenIsExpired && <Button variant="success" onClick={() => { refreshJwtAxios() }}>Refresh token</Button>
+          {tokenIsExpired &&
+            <Row>
+              <Button variant="success" onClick={() => { refreshJwtAndDoCallback(() => console.log("nothing")) }}>Refresh token</Button>
+              <br />
+              <hr />
+            </Row>
+
           }
+        </Row>
+      }
+      {jwt && tokenIsExpired &&
+        <Row>
+          <Button variant="warning" onClick={() => fetchProtectedData(jwt)}>Hit Protected End Point And Refresh Token</Button>
+          <br />
+          <hr />
         </Row>
       }
       {jwt &&
         <Row>
-          <Button variant="warning" onClick={() => hitProtectedEndpoint()}>Hit Protected End Point</Button>
+          <Button variant="warning" onClick={() => _doFetchProtectedData(jwt)}>Hit Protected End Point</Button>
           <br />
           <hr />
-        </Row>
-      }
-      {refreshMsg &&
-        <Row>
-          <br />
-          <hr />
-          <h1>Refresh Token</h1>
-          <CopyBlock
-            text={refreshMsg}
-            // language={"txt"}
-            showLineNumbers
-            theme={dracula}
-            codeBlock
-          />
         </Row>
       }
       {pocJson &&
---
+++ react_client/src/hooks/useAuthCodeFlowPKCE.js
@@ -0,0 +1,147 @@
+import { useEffect, useState, useRef,useMemo } from "react";
+
+import jwt_decode from "jwt-decode";
+
+const useAuthCodeFlowPKCE = (clientId, audience, authorizeEndpoint, tokenEndpoint) => {
+    var axios = require("axios").default;
+
+
+    const [jwt, setJwt] = useState(null)
+    const prevJwt = useRef();
+    const [refreshToken, setRefreshToken] = useState(null)
+    const [jwtExpiration, setJwtExpiration] = useState(null)
+    
+    const [callbackAfterRefresh, setCallbackAfterRefresh] = useState(null)
+
+    useEffect(() => {
+        var args = new URLSearchParams(window.location.search);
+        var code = args.get("code");
+
+        if (!code) { startOauthFlow(clientId, audience, authorizeEndpoint) }
+    }, [clientId, audience, authorizeEndpoint])
+
+    useEffect(() =>{
+        if (callbackAfterRefresh){
+          callbackAfterRefresh(jwt)
+
+          setCallbackAfterRefresh(null)
+        }
+
+      
+    }, [jwt])
+
+
+
+    useEffect(() => {
+        if (window.location.search) {
+          var args = new URLSearchParams(window.location.search);
+          var code = args.get("code");
+    
+          if (code) {
+           
+            var accessTokenOptions = {
+              method: 'POST',
+              url: tokenEndpoint,
+              headers: { 'Content-Type': 'application/x-www-form-urlencoded', Accept: '*/*' },
+              data: `client_id=${clientId}&code_verifier=${window.sessionStorage.getItem("code_verifier")}&grant_type=authorization_code&redirect_uri=${window.location.href.replace(window.location.search, '')}&code=${code}`
+            };
+        
+            axios.request(accessTokenOptions)
+              .then(function (response) {
+        
+                const { access_token, refresh_token } = response.data
+
+                setJwt(access_token)
+                prevJwt.current = access_token
+
+                setRefreshToken(refresh_token)
+                setJwtExpiration(getJwtExpiration(access_token))
+
+    
+              }).catch(function (error) {
+                console.error(error)
+                window.location.href = window.location.href.replace(window.location.search, '')
+              });
+          }
+        }
+      }, [])
+
+
+    function startOauthFlow(clientId, audience, authorizeEndpoint) {
+        var codeVerifier = generateRandomString(64);
+
+        generateCodeChallenge(codeVerifier).then(function (codeChallenge) {
+            window.sessionStorage.setItem("code_verifier", codeVerifier);
+
+            var redirectUri = window.location.href.split('?')[0];
+            var args = new URLSearchParams({
+                response_type: "code",
+                client_id: clientId,
+                code_challenge_method: "S256",
+                code_challenge: codeChallenge,
+                redirect_uri: redirectUri,
+                audience: audience,
+                scope: "offline_access" //To enable refresh token
+            });
+            window.location = authorizeEndpoint + "/?" + args;
+        });
+    }
+
+    function generateRandomString(length) {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < length; i++) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
+    }
+
+    async function generateCodeChallenge(codeVerifier) {
+        var digest = await crypto.subtle.digest("SHA-256",
+            new TextEncoder().encode(codeVerifier));
+
+        return btoa(String.fromCharCode(...new Uint8Array(digest)))
+            .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+    }
+
+    function getJwtExpiration(access_code){
+      const decodedJwt = jwt_decode(access_code)
+
+      const expDate = new Date(0);
+      expDate.setUTCSeconds(decodedJwt.exp);
+      return expDate
+    }
+
+    function refreshJwtAndDoCallback(callback) {
+
+        var refreshOptions = {
+          method: 'POST',
+          url: tokenEndpoint,
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded', Accept: '*/*' },
+          data: `grant_type=refresh_token&client_id=${clientId}&refresh_token=${refreshToken}`
+        };
+    
+        axios.request(refreshOptions)
+          .then(function (response) {
+    
+            const { access_token, refresh_token } = response.data
+    
+            setCallbackAfterRefresh(() => callback)
+            setJwt(access_token)
+            setRefreshToken(refresh_token)            
+            setJwtExpiration(getJwtExpiration(access_token))
+
+          }).catch(function (error) {
+            console.error(error);
+            alert(error)
+          });
+
+    }
+
+    return [jwt, jwtExpiration, refreshJwtAndDoCallback]
+
+}
+
+export default useAuthCodeFlowPKCE
